dee53cc52744bc97a45af00927523544a0beffd5
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2026-08-24 14:10:31 -0500

    Added Output
    Made the calculations and output the remaining money.

--- Chapter1/lessons/Budget/WeeklyBudget.java
+++ Chapter1/lessons/Budget/WeeklyBudget.java
@@ -33,8 +33,14 @@ public class WeeklyBudget
         System.out.print("Enter the number of school lunches you ordered this week: ");
         lunchesPerWeek = scan.nextInt();
         
+        // Perform calculations (Arithmetic expressions)
+        totalCost = lunchPrice * lunchesPerWeek;
+        remaining = allowance - totalCost;
         
-        
+        System.out.println(allowance);
+        System.out.println(totalCost);
+        System.out.println(remaining);
+                
         
         
         
