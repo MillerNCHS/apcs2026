aba0a78dbf70cc8f76f884929d2b1e09a3392ede
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2026-08-27 14:11:16 -0500

    Finished Prize Generator
    Complete the Prize Generator program by adding output and calculating the new balance.

--- Chapter2/lessons/Prize/PrizeGenerator.java
+++ Chapter2/lessons/Prize/PrizeGenerator.java
@@ -32,6 +32,28 @@ public class PrizeGenerator
         // Generate a random fee between $1 and $5
         int fee = (int)(Math.random() * 5) + 1;
         
+        // int values are automatically promoted to double
+        // This is allowed because no information is lost
+        double finalBalance = startBalance + prizeAmount - fee;
+        
+        System.out.println("\n Congratulations " + name + "! Here is your statement:");
+        System.out.println("===============================================");
+        
+        System.out.printf("%-25s %s%n", "Starting Balance", money.format(startBalance));
+        System.out.printf("%-25s %s%n", "Prize Money", money.format(prizeAmount));
+        System.out.printf("%-25s %s%n", "Processing Fee", money.format(fee));
+        System.out.println("---------------------------------------");
+        System.out.printf("%-25s %s%n", "Final Balance", money.format(finalBalance));
+        System.out.println("===============================================");
+        
+     
+        
+        
+        
+        
+        
+        
+        
         
         
         
