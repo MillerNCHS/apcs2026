5cf554db647bd6a826d76b116f72d2a6da367b24
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2026-09-22 14:52:59 -0500

    Finished Movie Ticket Prices
    Added IMAX tickets and used nested if statements as well as else if statements.

--- Chapter3/Movie Tickets/MovieTicketPricer.java
+++ Chapter3/Movie Tickets/MovieTicketPricer.java
@@ -16,11 +16,20 @@ public class MovieTicketPricer
         // A constant variable CAN'T be changed
         final double REGULAR_PRICE = 12.50;
         final double DISCOUNT_PRICE = 8.00;
+        final double IMAX_SURCHARGE = 5.00;
+        final double IMAX_70MM_SURCHARGE = 8.00;
         
         Scanner scan = new Scanner(System.in);
         NumberFormat money = NumberFormat.getCurrencyInstance();
         
         System.out.println("---Movie Ticket Calculator---");
+        System.out.println("Select Movie Format:");
+        System.out.println("1 - Standard Format");
+        System.out.println("2 - IMAX");
+        System.out.println("3 - IMAX 70mm (As Nolan Intended)");
+        System.out.print("Enter choice (1-3): ");
+        int format = scan.nextInt();
+        
         System.out.print("Enter the customer's age: ");
         int age = scan.nextInt();
         
@@ -38,28 +47,32 @@ public class MovieTicketPricer
         
         double ticketPrice;
         
-        // Discount applies if <13, >= 65 OR its a matinee and they have a pass
-        if (age < 13 || age >= 65 || (matinee && pass)) {
-            ticketPrice = DISCOUNT_PRICE;
-            System.out.println("Status: Discount Applied!");
+        if (format == 1) {
+            // Discount applies if <13, >= 65 OR its a matinee and they have a pass
+            if (age < 13 || age >= 65 || (matinee && pass)) {
+                ticketPrice = DISCOUNT_PRICE;
+                System.out.println("Status: Discount Applied!");
+            }
+            else {
+                ticketPrice = REGULAR_PRICE;
+                System.out.println("Status: Regular Rate Applied.");
+            
+            }
+        }
+        else if (format == 2) {
+            ticketPrice = REGULAR_PRICE + IMAX_SURCHARGE;
+            System.out.println("Status: IMAX Surcharge Applied.");    
+        }
+        else if (format == 3) {
+            ticketPrice = REGULAR_PRICE + IMAX_70MM_SURCHARGE;
+            System.out.println("Status: IMAX 70mm Surcharge Applied.");                
         }
         else {
             ticketPrice = REGULAR_PRICE;
-            System.out.println("Status: Regular Rate Applied.");
+            System.out.println("Status: Incorrect entry. Regular Price Applied.");                
         
         }
         
         System.out.println("Total Due: " + money.format(ticketPrice));
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
 }
